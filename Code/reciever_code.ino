16716407050b08d2397dceaa65b5d1c98d1e41ba
ChavvaHiteshReddy <chavvahitesh824@gmail.com>
2025-02-05 13:31:35 +0530

    Update reciever_code.ino

--- Code/reciever_code.ino
+++ Code/reciever_code.ino
@@ -1,47 +1,15 @@
-#define LDR_PIN A6
-#define LED A4
-#define SAMPLING_TIME 20
+#define LDR_PIN A0  // LDR/Photodiode sensor
+#define SPEAKER_PIN 9 // Speaker output
 
-
-
-//Declaration
-bool led_state = false;
-bool previous_state = true;
-bool current_state = true;
-char buff[64];
-void setup() 
-{
-  pinMode(LED,OUTPUT);
+void setup() {
+  pinMode(SPEAKER_PIN, OUTPUT);
   Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(LDR_PIN); // Read Li-Fi signal
+  int frequency = map(sensorValue, 0, 1023, 200, 1000); // Convert light intensity to frequency
   
-}
-
-void loop() 
-{
-  current_state = get_ldr(); 
-  if(!current_state && previous_state)
-  {
-    sprintf(buff, "%c", get_byte());
-    Serial.print(buff);
-  }
-  digitalWrite(LED, current_state);
-  previous_state = current_state;
-}
-bool get_ldr()
-{
-  bool val = analogRead(LDR_PIN) > 90 ? true : false;
-  digitalWrite(LED, val);
-  return val;
-}
-
-char get_byte()
-{
-  char data_byte = 0;
-  delay(SAMPLING_TIME * 1.5);
-  for(int i = 0; i < 8; i++)
-  {
-    data_byte = data_byte | (char)get_ldr() << i;
-    delay(SAMPLING_TIME);
-  }
-  return data_byte;
-}
+  // Play tone based on received signal
+  tone(SPEAKER_PIN, frequency, 100);
+  Serial.println(sensorValue); // Debugging the received signal
