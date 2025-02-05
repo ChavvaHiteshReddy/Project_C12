f7f1f6b35ee1c3ad8d6527b8164816168d34c4c3
ChavvaHiteshReddy <chavvahitesh824@gmail.com>
2025-02-05 13:30:49 +0530

    Update transmiter_code.ino

--- Code/transmiter_code.ino
+++ Code/transmiter_code.ino
@@ -1,49 +1,26 @@
-#define TRANSMIT_LED 11
-#define BUTTON 3
-#define SAMPLING_TIME 20
+#include <SoftwareSerial.h>
+#define keyy A4
+SoftwareSerial mySerial(10, 11); // RX, TX
 
-char* text = "Li-Fi is wireless communication technology which utilizes light to transmit data and position between devices. The term was first introduced by Harald Haas during a 2011 TEDGlobal talk in Edinburgh.";
-
-//Declaration
-bool led_state = false;
-bool button_state = false;
-bool transmit_data = true;
-int bytes_counter = 20;
-int total_bytes;
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(TRANSMIT_LED,OUTPUT);
-  //pinMode(BUTTON,INPUT_PULLUP);
-  total_bytes = strlen(text);
+  // Start serial communications
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(keyy, OUTPUT);
+  digitalWrite(keyy, HIGH);
+  Serial.println("Type something to send the data in LIFI!");
 }
 
 void loop() {
- //button_state=!digitalRead(BUTTON);
-
-   while(transmit_data)
-   {
-    transmit_byte(text[total_bytes - bytes_counter]);
-    bytes_counter--;
-      if(bytes_counter == 0)
-      {
-        transmit_data = false;
-      }
-     //delay(100);
-   }
-   transmit_data = true;
-   bytes_counter = total_bytes;
-    delay(1000);
- }
-
-void transmit_byte(char data_byte)
-{
-  digitalWrite(TRANSMIT_LED,LOW); 
-  delay(SAMPLING_TIME);
-  for(int i = 0; i < 8; i++)
-  {
-    digitalWrite(TRANSMIT_LED,(data_byte >> i) & 0x01); 
-    delay(SAMPLING_TIME);
+  // Send data from Serial Monitor to SoftwareSerial device
+  if (Serial.available()) {
+    char inChar = Serial.read();
+    mySerial.write(inChar);
   }
- digitalWrite(TRANSMIT_LED,HIGH); //Return to IDLE state
- delay(SAMPLING_TIME);
+
+  // Send data from SoftwareSerial device to Serial Monitor
+  if (mySerial.available()) {
+    char inChar = mySerial.read();
+    Serial.write(inChar);
+  }   
 }
